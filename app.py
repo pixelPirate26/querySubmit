2694a7800f4166a508d140a248be6c834b4f2abc
Ashwin K Sharma <ashwinksharma01@gmail.com>
2024-05-20 00:42:58 +0530

    add css for index.html

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, session, url_for, f
 import psycopg2
 from collections import namedtuple
 from werkzeug.exceptions import MethodNotAllowed
+import json
 
 app = Flask(__name__)
 app.secret_key = 'SpeakQL_Portal'
@@ -9,7 +10,7 @@ app.secret_key = 'SpeakQL_Portal'
 # PostgreSQL connection details
 conn = psycopg2.connect(
     host="localhost",
-    database="banavo2",
+    database="test",
     user="test",
     password="test",
     port=5432 
@@ -33,7 +34,7 @@ def login_required(func):
 @app.before_request
 def make_session_permanent():
     session.permanent = True
-    app.permanent_session_lifetime = 120  # Session lifetime in seconds 
+    app.permanent_session_lifetime = 180  # Session lifetime in seconds 
 
 @app.teardown_appcontext
 def remove_session(exception=None):
@@ -50,118 +51,10 @@ def index():
         elif session['role'] == 'validator':
             return redirect('/validate')
         
-    particles_config = {
-        "particles": {
-            "number": {
-                "value": 90,
-                "density": {
-                    "enable": True,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#4caf50"
-            },
-            "shape": {
-                "type": "star",
-                "stroke": {
-                    "width": 0.7,
-                    "color": "#bbdebd"
-                },
-                "polygon": {
-                    "nb_sides": 12
-                },
-                "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-                }
-            },
-            "opacity": {
-                "value": 1,
-                "random": True,
-                "anim": {
-                    "enable": False,
-                    "speed": 0.2,
-                    "opacity_min": 0,
-                    "sync": False
-                }
-            },
-            "size": {
-                "value": 2.8,
-                "random": True,
-                "anim": {
-                    "enable": False,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": False
-                }
-            },
-            "line_linked": {
-                "enable": True,
-                "distance": 200,
-                "color": "#4caf50",
-                "opacity": 0.4,
-                "width": 1
-            },
-            "move": {
-                "enable": True,
-                "speed": 6,
-                "direction": "none",
-                "random": False,
-                "straight": False,
-                "out_mode": "out",
-                "bounce": False,
-                "attract": {
-                    "enable": True,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
-        },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": True,
-                    "mode": "bubble"
-                },
-                "onclick": {
-                    "enable": True,
-                    "mode": "repulse"
-                },
-                "resize": True
-            },
-            "modes": {
-                "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 36,
-                    "duration": 2,
-                    "opacity": 0.5,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 250,
-                    "duration": 0.4
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
-            }
-        },
-        "retina_detect": True
-    }
+    with open('static/particlesConfigLogin.json', 'r') as file:
+        particles_config_login = json.load(file)
 
-    return render_template('login.html', particles_config=particles_config)
+    return render_template('login.html', particles_config = particles_config_login)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -184,36 +77,71 @@ def login():
 @app.route('/submit_query', methods=['GET', 'POST'])
 @login_required
 def submit_query():
+    with open('static/particlesConfigQuery.json', 'r') as file:
+        particles_config_query = json.load(file)
+
     if request.method == 'GET' and session['role'] != 'questioner':
         return redirect(url_for('index'))
 
     if request.method == 'POST':
-        if 'execute' in request.form:  # Execute the query
+        if 'execute' in request.form:
             query = request.form['query'].strip()
+            question = request.form['question'].strip()
             if not query:
-                flash('Query cannot be empty')
+                flash('Query cannot be empty', 'error')
                 return redirect(url_for('submit_query'))
-
+            if not question:
+                flash('Question cannot be empty', 'error')
+                return redirect(url_for('submit_query'))
             try:
                 with conn.cursor() as cursor:
                     cursor.execute(query)
-                    result = cursor.fetchall()  # Get the query result
-                conn.commit()  # Commit the transaction if the query is successful
+                    result = cursor.fetchall()
+                    conn.commit()
+                    column_names = [desc[0] for desc in cursor.description]  # Get column names
             except psycopg2.Error as e:
-                conn.rollback()  # Rollback the transaction if there's an error
+                conn.rollback()
                 flash(f'Error executing query: {e}')
                 return redirect(url_for('submit_query'))
 
-            return render_template('index.html', query=query, result=result, question=request.form['question'])
+            if result:
+                result_str = '<table>\n'
+                result_str += '  <tr>\n'
+                # Get column names from cursor.description
+                column_names = [desc[0] for desc in cursor.description]
+                for col_name in column_names:
+                    result_str += f'        <th>{col_name}</th>\n'
+                result_str += '  </tr>\n'
+
+                # Loop through each row and display data in table cells
+                for row in result:
+                    result_str += '  <tr>\n'
+                    for value in row:
+                        result_str += f'    <td>{value}</td>\n'
+                    result_str += '  </tr>\n'
+
+                result_str += '</table>\n'
+            else:
+                '''result_str = '<table>\n'
+                result_str += '  <tr>\n'
+                for col_name in column_names:
+                    result_str += f'    <th>{col_name}</th>\n'
+                result_str += '  </tr>\n'
+                result_str += '</table>\n'''
+                result_str = '<p>No results found.</p>'
+
+            return render_template('index.html', query=query, result=result_str, question=request.form['question'],particles_config=particles_config_query)
 
         elif 'submit' in request.form:  # Submit the query-question pair
             query = request.form['query'].strip()
             question = request.form['question'].strip()
 
             if not query:
-                return 'Query cannot be empty', 400
+                flash('Query cannot be empty', 'error')
+                return redirect(url_for('submit_query'))
             if not question:
-                return 'Question cannot be empty', 400
+                flash('Question cannot be empty', 'error')
+                return redirect(url_for('submit_query'))
 
             try:
                 with conn.cursor() as cursor:
@@ -228,14 +156,15 @@ def submit_query():
 
             pair_id = len(pairs) + 1
             pairs.append(QueryPair(pair_id, query, question, session['username']))
-            return 'Query-question pair submitted successfully'
+            flash('Query-question pair submitted successfully!', 'success')
+
 
     # Check if the route was accessed with pre-populated data
     query = request.args.get('query', '')
     question = request.args.get('question', '')
     remarks = request.args.get('remarks', '')
 
-    return render_template('index.html', query=query, question=question, remarks=remarks)
+    return render_template('index.html', query=query, question=question, remarks=remarks, particles_config=particles_config_query )
 
 @app.route('/validate', methods=['GET', 'POST'])
 @login_required
